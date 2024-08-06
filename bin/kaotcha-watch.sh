f67107f7bd894d385f2a8c9a66380cf88ea798b4
Ivar Refsdal <refsdal.ivar@gmail.com>
2024-08-06 14:46:33 +0200

    Start write own reporter

--- bin/kaotcha-watch.sh
+++ bin/kaotcha-watch.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-printf "./bin/kaocha\ntests.edn\ntest/com/github/ivarref/kaocha_hook.clj\ndeps.edn" | entr -r ./bin/kaocha
+printf "./bin/kaocha\ntests.edn\ntest/com/github/ivarref/kaocha_hook.clj\ndeps.edn\ntest/com/github/ivarref/reporter.clj\n" | \
+entr -r ./bin/kaocha
---
+++ test/com/github/ivarref/reporter.clj
@@ -0,0 +1,1 @@
+(ns com.github.ivarref.reporter)
